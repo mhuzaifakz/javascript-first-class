baf0ee4c11965ba8072620a7e84b37399b7c3b11
Muhammad Huzaifa <m.huzaifa.khandaza@icloud.com>
2026-02-02 18:42:06 +0500

    i have learned how to use github copilot

---
+++ github-founders.js
@@ -0,0 +1,12 @@
+// Founders of GitHub
+const githubFounders = [
+    'Tom Preston-Werner',
+    'Chris Wanstrath',
+    'PJ Hyett',
+    'Scott Chacon'
+];
+
+// Export for use in other scripts if needed
+if (typeof module !== 'undefined') {
+    module.exports = githubFounders;
+}
